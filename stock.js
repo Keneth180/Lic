f819a6ac27d49b57a4a4d08d1c55d75a9f91019e
Keneth180 <86987550+Keneth180@users.noreply.github.com>
2022-11-11 17:33:45 -0500

    Add files via upload

--- stock.js
+++ stock.js
@@ -4,7 +4,7 @@ let stockProductos = [
     {id: 3, nombre: "HEINIKEN",  cantidad: 1,  precio: 12000,  img: './resources/heiniken.png'},
     {id: 4, nombre: "CORONA EXTRA",  cantidad: 1,  precio: 20000,  img: './resources/corona.png'},
     {id: 5, nombre: "Buchanans",  cantidad: 1,  precio: 120000,  img: './resources/BUCHANANS.png'},
-    {id: 6, nombre: "Chivas Regal",  cantidad: 1,  precio: 105000,  img: './resources/ChivasR.png'},
+    {id: 6, nombre: "Chivas Regal",  cantidad: 1,  precio: 105000,  img: './resources/chivasR.png'},
     {id: 7, nombre: "Monkey Shoulder",  cantidad: 1,  precio: 125000,  img: './resources/mks.png'},
     {id: 8, nombre: "Grants",  cantidad: 1,  precio: 75000,  img: './resources/grants.png'},
 
