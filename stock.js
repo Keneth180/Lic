2f6bc301d61ad604ce18691990a38b7cc776ce57
Keneth180 <86987550+Keneth180@users.noreply.github.com>
2022-11-12 17:15:39 -0500

    Update stock.js

--- stock.js
+++ stock.js
@@ -1,7 +1,7 @@
 let stockProductos = [
     {id: 1, nombre: "POKER",  cantidad: 1,  precio: 12000,  img: './resources/poker.png'},
     {id: 2, nombre: "CLUB COLOMBIA",  cantidad: 1,  precio: 13000,  img: './resources/clubcol.png'},
-    {id: 3, nombre: "HEINIKEN",  cantidad: 1,  precio: 12000,  img: './resources/domecq.png'},
+    {id: 3, nombre: "HEINIKEN",  cantidad: 1,  precio: 12000,  img: './resources/BUCHANANS.png'},
     {id: 4, nombre: "CORONA EXTRA",  cantidad: 1,  precio: 20000,  img: './resources/corona.png'},
     {id: 5, nombre: "Buchanans",  cantidad: 1,  precio: 120000,  img: './resources/BUCHANANS.png'},
     {id: 6, nombre: "Chivas Regal",  cantidad: 1,  precio: 105000,  img: './resources/chivasR.png'},
